bee0e192637723c51f982737fc57bfe6fef474fb
yajingwang1022 <wang.yaj@husky.neu.edu>
2021-02-05 15:08:56 -0500

    Added stack and tab navigations and basic screens.

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import { AppRegistry } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import BottomTabNavigator from "./navigation/TabNavigator";
 
-export default function App() {
-  return (
-    <WelcomeScreen />
-  )
+// const store = createStore(AppReducer, applyMiddleware(middleware));
+class StarterApp extends React.Component {
+  render() {
+    return (
+      // <Provider store={store}>
+        <NavigationContainer>
+          <BottomTabNavigator />
+        </NavigationContainer>
+      // </Provider>
+      
+    );
+  }
 }
 
+AppRegistry.registerComponent("StarterApp", () => StarterApp);
+
+export default StarterApp;
