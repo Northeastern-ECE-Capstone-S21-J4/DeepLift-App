e098c6d4a06fbdb29aa221991ab30e6387acb47a
shorstman <horstman.s@husky.neu.edu>
2021-02-10 07:37:41 -0500

    Removing node_modules again and fixing merge conflict in App.js

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { AppRegistry } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./navigation/TabNavigator";
 
-<<<<<<< HEAD
 // TO DO: need to do more research on Provider
 // const store = createStore(AppReducer, applyMiddleware(middleware));
 class StarterApp extends React.Component {
@@ -17,28 +16,6 @@ class StarterApp extends React.Component {
       
     );
   }
-=======
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Let's go DeepLift!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-<<<<<<< HEAD
-=======
-=======
-import RecordingScreen from './app/screens/RecordingScreen';
-import SetInformationScreen from './app/screens/SetInformationScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-
-export default function App() {
-  return (
-    <SetInformationScreen />
-  )
->>>>>>> 90344bb5... Basic Camera code
->>>>>>> caf7ec97... fixing some screwy commits
->>>>>>> dropping commit from other branch that somehow ended up here
 }
 
 AppRegistry.registerComponent("StarterApp", () => StarterApp);
