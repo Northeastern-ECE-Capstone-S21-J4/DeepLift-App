7f8c0a37628340de7274ec00894a866b42c10747
Nicholas Fresneda <nicholasfresneda@gmail.com>
2021-02-14 22:19:29 -0500

    new amplify changes

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { AppRegistry } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./navigation/TabNavigator";
 
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+
 // TO DO: need to do more research on Provider
 // const store = createStore(AppReducer, applyMiddleware(middleware));
 class StarterApp extends React.Component {
