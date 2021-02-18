f4b0559859c795b1d58363b93240e21a322139f1
shorstman <horstman.s@husky.neu.edu>
2021-02-18 14:17:19 -0500

    Adding specific helper functions for each endpoint

--- App.js
+++ App.js
@@ -6,12 +6,17 @@ import BottomTabNavigator from "./navigation/TabNavigator";
 
 // TO DO: need to do more research on Provider
 // const store = createStore(AppReducer, applyMiddleware(middleware));
-var apiHelper = new APIHelper("http://127.0.0.1:8000");
+var apiHelper = new APIHelper("https://api.deepliftcapstone.xyz");//"http://127.0.0.1:8000");
 
 class StarterApp extends React.Component {
   async componentDidMount(){
     await apiHelper.getToken("sam", "3AYnpU7p46fr");
-    var users = apiHelper.getRequest("/users");
+    apiHelper.getUsers();
+    apiHelper.getUserById(1);
+    apiHelper.getWorkoutById(1);
+    apiHelper.getUserWorkouts(1);
+    apiHelper.getUserExerciseById(1, 1);
+    apiHelper.getExercises();
   }
 
   render() {
