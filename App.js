0b09551ecbbb55ee71027ca371894bc51a63207e
shorstman <horstman.s@husky.neu.edu>
2021-02-18 13:54:06 -0500

    Basic functions for querying API

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { AppRegistry } from "react-native";
+import { APIHelper } from "./lib/api/api.js"
+import { AppRegistry, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./navigation/TabNavigator";
 
 // TO DO: need to do more research on Provider
 // const store = createStore(AppReducer, applyMiddleware(middleware));
+var apiHelper = new APIHelper("http://127.0.0.1:8000");
+
 class StarterApp extends React.Component {
+  async componentDidMount(){
+    await apiHelper.getToken("sam", "3AYnpU7p46fr");
+    var users = apiHelper.getRequest("/users");
+  }
+
   render() {
     return (
       // <Provider store={store}>
