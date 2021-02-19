3bf96da6881f15d33faaf021844191290b514208
yajingwang1022 <wang.yaj@husky.neu.edu>
2021-02-19 01:53:18 -0500

    Added a function to retrieve the current user info.

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { AppRegistry } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./navigation/TabNavigator";
 
-import Amplify from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import config from './aws-exports';
 import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react-native';
 
@@ -11,6 +11,7 @@ Amplify.configure(config)
 
 const MyButton = Object.assign({}, AmplifyTheme.button, { backgroundColor: '#9AC4F8', textAlign: 'center', verticalAlign: 'middle', height: '60px', borderRadius: '5px',});
 const MyTheme = Object.assign({}, AmplifyTheme, { button: MyButton });
+
 class StarterApp extends React.Component {
   render() {
     return (
@@ -19,6 +20,12 @@ class StarterApp extends React.Component {
         </NavigationContainer>
     );
   }
+}
+
+async function getCurrentUser() {
+  const { attributes } = await Auth.currentAuthenticatedUser();
+  const userEmail = attributes.email;
+  return userEmail;
 }
 
 AppRegistry.registerComponent("StarterApp", () => StarterApp);
