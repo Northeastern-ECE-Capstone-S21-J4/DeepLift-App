590b69af300af634d5feb51b73db9785a0c5d0b5
yajingwang1022 <wang.yaj@husky.neu.edu>
2021-03-01 17:33:20 -0500

    Connected to the back-end and displayed exercises on the home page.

--- screens/Home.js
+++ screens/Home.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
 
 const Home = ({ navigation }) => {
+
+  const [exercises, setExercises] = useState([]);
+  const fetchExercises = async () => {
+    const response = await fetch("https://api.deepliftcapstone.xyz/exercises");
+    const exercises = await response.json();
+    exercises.sort((a, b) => a.exerciseID - b.exerciseID);
+    console.log(exercises);
+    setExercises(exercises);
+  }
+  useEffect(() => {
+    fetchExercises()
+  }, [])
+  
   return (
-    <View style={styles.center}>
-      <Text>This is the home screen</Text>
-      <Button
-        title="Start Workout"
-        onPress={() => navigation.navigate("About")}
-      />
+    <View style={styles.container}>
+      {exercises.map((exercise) => {return <Button title={exercise.exerciseName} 
+                                            key={exercise.exerciseID} 
+                                            style={styles.exerciseButton}
+                                            onPress={() => navigation.navigate("About")}/>})}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  center: {
+  container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
   },
+  exerciseButton: {
+    color: '#9AC4F8'
+  }
 });
 
 export default Home;
