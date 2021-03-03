c13813d8ce98fec68a428318c434c1f2384a441b
shorstman <horstman.s@husky.neu.edu>
2021-03-03 14:53:31 -0500

    Beginning login system

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { APIHelper } from "./lib/api/api.js"
+import { User } from "./lib/user/user.js"
 import { AppRegistry } from "react-native";
+import Login from "./screens/Login.js"
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./navigation/TabNavigator";
+import SetInformationScreen from './screens/SetInformationScreen.js';
 
-var apiHelper = new APIHelper("https://api.deepliftcapstone.xyz");//"http://127.0.0.1:8000");
+var apiHelper = new APIHelper("https://api.deepliftcapstone.xyz");//"http://127.0.0.1:8000");//
+var user = new User("bigjohn", "password")
 
 class StarterApp extends React.Component {
   async componentDidMount(){
-    await apiHelper.getToken("username", "password");
+    apiHelper.login(user)
     apiHelper.getUsers();
-    apiHelper.getUserById(1);
-    apiHelper.getWorkoutById(1);
-    apiHelper.getUserWorkouts(1);
-    apiHelper.getUserExerciseById(1, 1);
-    apiHelper.getExercises();
   }
 
   render() {
     return (
-        <NavigationContainer>
-          <BottomTabNavigator />
-        </NavigationContainer>
+      <Login />
     );
   }
 }
