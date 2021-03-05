fe82b3088eb8a812a8c415171ae6271835f325cd
yajingwang1022 <wang.yaj@husky.neu.edu>
2021-03-04 23:54:12 -0500

    I need help - I'm confused...

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
+import { APIHelper } from "../lib/api/api";
 
 const Home = ({ navigation }) => {
 
   const [exercises, setExercises] = useState([]);
   const fetchExercises = async () => {
-    const response = await fetch("https://api.deepliftcapstone.xyz/exercises");
-    const exercises = await response.json();
-    exercises.sort((a, b) => a.exerciseID - b.exerciseID);
+    // const response = await fetch("https://api.deepliftcapstone.xyz/exercises");
+    var apiHelper = new APIHelper("https://api.deepliftcapstone.xyz");
+    const exercises = await apiHelper.getExercises();
+    // exercises.sort((a, b) => a.exerciseID - b.exerciseID);
     console.log(exercises);
     setExercises(exercises);
   }
