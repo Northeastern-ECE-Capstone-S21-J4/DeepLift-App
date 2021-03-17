93364fc457817ae3ef00f5519ced07652dcd1141
yajingwang1022 <wang.yaj@husky.neu.edu>
2021-03-17 01:14:53 -0400

    Added Video components on the past workout page.

--- screens/PastWorkouts.js
+++ screens/PastWorkouts.js
@@ -1,11 +1,35 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { ScrollView, StyleSheet, Text, Dimensions, Button } from "react-native";
+import { Video, AVPlaybackStatus } from 'expo-av';
 
 const PastWorkouts = () => {
+  const { width, height } = Dimensions.get('window');
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>Past Workouts: </Text>
-    </View>
+      <Video
+        ref={video}
+	      source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+        shouldPlay={false}
+	      useNativeControls
+        resizeMode="cover"
+        isLooping={false}
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+	      style={{ width: 0.8*width, height: 200, margin: "10%" }}
+	    />
+      <Video
+        ref={video}
+	      source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+        shouldPlay={false}
+	      useNativeControls
+        resizeMode="cover"
+        isLooping={false}
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+	      style={{ width: 0.8*width, height: 200, marginHorizontal: "10%" }}
+	    />
+    </ScrollView>
   );
 };
 
@@ -14,13 +38,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#31373b',
     fontSize: 20,
-    margin: "5%"
+    margin: "5%",
+    marginBottom: 0
   },
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
+    flexDirection: 'column'
   },
 });
 
