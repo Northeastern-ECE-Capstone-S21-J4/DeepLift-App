4e2853fb3d2a52034102eab07bf575a714366e33
yajingwang1022 <wang.yaj@husky.neu.edu>
2021-03-24 13:27:01 -0400

    Started working on the profile page.

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet, Image, Button, Text } from "react-native";
 import { Auth } from 'aws-amplify';
+import { Ionicons } from '@expo/vector-icons';
 
 const Profile = () => {
   return (
     <View style={styles.container}>
-      <Button title="Sign Out" color="red" onPress={signOut}/>
+      <View style={styles.profilePic}>
+        <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+        <View style={styles.userInfo}>
+          <Text style={styles.username}>Yajing Wang</Text>
+          <Ionicons name="location-sharp" style={styles.location}> Boston, MA</Ionicons>
+        </View>
+      </View>
+      <Button title="Delete Account" color="red" onPress={()=>{}}/>
+      <Button title="Log Out" color="red" onPress={signOut}/>
     </View>
   );
 };
@@ -20,9 +29,38 @@ async function signOut() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     flexDirection: 'column',
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
     alignItems: 'stretch',
+  },
+  profilePic: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'stretch',
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: "white",
+    marginVertical: "10%",
+  },
+  userInfo: {
+    marginVertical: "13%",
+  },
+  username: {
+    fontWeight: "bold",
+    fontSize: 20,
+    margin: "2%",
+    textAlign: "center"
+  },
+  location: {
+    fontSize: 15,
+    color: "gray",
+    margin: "2%",
+    textAlign: "center"
   },
 });
 
