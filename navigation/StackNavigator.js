78b6de566be33328e0a75b37b265a871d30a4be1
Sam Horstman <horstman.s@husky.neu.edu>
2021-03-31 12:18:21 -0400

    Adding back import that got lost in rebase

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -7,6 +7,7 @@ import Profile from "../screens/Profile";
 import PastWorkouts from "../screens/PastWorkouts";
 import QRCodePage from "../screens/QRCodePage";
 import LoadingPage from "../screens/LoadingPage";
+import LoginScreen from "../screens/Login";
 import Signup from "../screens/Signup";
 
 const Stack = createStackNavigator();
